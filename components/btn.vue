1dbefdbacc9d750f55f906c8c723bd056778e30e
ohiosveryown <matt@ohiosveryown.co>
2022-12-10 16:36:04 +0000

    initial commit - reset, text styles, button

---
+++ components/btn.vue
@@ -0,0 +1,38 @@
+<template>
+  <button :class="type" class="fs-xs inter">
+    {{ content }}
+  </button>
+</template>
+
+<style lang="scss" scoped>
+  @import "~static/style/grid.scss";
+
+  button {
+    padding: 0.6rem 1.6rem 0.7rem;
+    border-radius: 100px;
+    color: #fff;
+    font-weight: 500;
+    text-transform: capitalize;
+    transition: filter 300ms ease;
+  }
+
+  @media (pointer: fine) {
+    button:hover {
+      filter: hue-rotate(20deg);
+    }
+  }
+
+  .primary {
+    background: var(--gradient-brand);
+  }
+
+  .secondary {
+    background: red;
+  }
+</style>
+
+<script>
+  export default {
+    props: ["type", "content"],
+  }
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="width app">
+    <btn :type="{ primary: true }" content="button label" />
+
+    <h1 class="fs-xxl">
+      Heading Number <br />
+      One
+    </h1>
+
+    <h2 class="fs-xl">
+      Heading Number <br />
+      Two
+    </h2>
+
+    <h3 class="fs-lg founders">
+      Heading Number <br />
+      Three
+    </h3>
+
+    <h4 class="inter fs-md">
+      Heading Number <br />
+      Four
+    </h4>
+
+    <h5 class="fs-sm">
+      Heading Number <br />
+      Five
+    </h5>
+
+    <h6 class="fs-xs">
+      Heading Number <br />
+      Six
+    </h6>
+
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium
+      perferendis reprehenderit quasi laudantium voluptate officia vero
+      recusandae nostrum voluptates, provident accusantium eum cum amet
+      laboriosam magnam minus harum aliquam molestiae.
+    </p>
+
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium
+      perferendis reprehenderit quasi laudantium voluptate officia vero
+      recusandae nostrum voluptates, provident accusantium eum cum amet
+      laboriosam magnam minus harum aliquam molestiae.
+    </p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  @import "~static/style/grid.scss";
+
+  .app h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    margin-bottom: 4rem;
+  }
+</style>
+
+<script>
+  export default {}
+</script>
